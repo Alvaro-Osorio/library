ac7398a9f967a84a8a59fa3e5b7885e7d40cb443
Alvaro Osorio <alvaroosorioiba@gmail.com>
2024-01-03 22:33:34 -0600

    Arreglando implementaciones

--- src/main/java/com/app/ao/Service/Impl/EditorialServiceImpl.java
+++ src/main/java/com/app/ao/Service/Impl/EditorialServiceImpl.java
@@ -1,7 +1,7 @@
 package com.app.ao.Service.Impl;
 
 import com.app.ao.Entities.Editorial;
-import com.app.ao.Repository.EditorialRepository;
+import com.app.ao.Persistence.IEditorialDAO;
 import com.app.ao.Service.IEditorialService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,25 +12,25 @@ import java.util.Optional;
 @Service
 public class EditorialServiceImpl implements IEditorialService {
     @Autowired
-    private EditorialRepository editorialRepository;
+    private IEditorialDAO editorialDAO;
 
     @Override
     public Optional<Editorial> findById(Long id) {
-        return editorialRepository.findById(id);
+        return editorialDAO.findById(id);
     }
 
     @Override
     public List<Editorial> findAll() {
-        return ((List<Editorial>) editorialRepository.findAll());
+        return editorialDAO.findAll();
     }
 
     @Override
     public void save(Editorial editorial) {
-        editorialRepository.save(editorial);
+        editorialDAO.save(editorial);
     }
 
     @Override
     public void deleteById(Long id) {
-        editorialRepository.deleteById(id);
+        editorialDAO.deleteById(id);
     }
 }
